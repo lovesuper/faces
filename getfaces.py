7df43dee3ca998221f824bdeb34d4a714504e529
lovesuper <antononrails@gmail.com>
2020-06-30 11:12:43 +0300

    fix

--- getfaces.py
+++ getfaces.py
@@ -3,8 +3,8 @@ import os
 import cv2
 
 ix = 0
-for root, directories, file_names in os.walk('/Volumes/MacStorage/newdir'):
-    if ix > 10000:
+for root, directories, file_names in os.walk('/home/lovesuper/Documents/Photos/Male/Black'):
+    if ix > 10:
         break
 
     for filename in file_names:
@@ -12,14 +12,8 @@ for root, directories, file_names in os.walk('/Volumes/MacStorage/newdir'):
             imagePath = os.path.join(root, filename)
             image = cv2.imread(imagePath)
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            faceCascade = cv2.CascadeClassifier(
-                cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.01,
-                minNeighbors=6,
-                minSize=(150, 150)
-            )
+            faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+            faces = faceCascade.detectMultiScale(gray, scaleFactor=1.01, minNeighbors=1, minSize=(300, 300))
             print("[INFO] Found {0} Faces.".format(len(faces)))
             (x, y, w, h) = faces[0]
             roi_color = image[y:y + h, x:x + w]
